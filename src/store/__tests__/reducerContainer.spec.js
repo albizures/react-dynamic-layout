e7f580607c286e3329b8ec5bb96408a8d9ba23fb
albizures <albizures3601@gmail.com>
2017-01-24 21:46:49 -0600

    test(container): fully tested container reducer

--- src/store/__tests__/reducerContainer.spec.js
+++ src/store/__tests__/reducerContainer.spec.js
@@ -3,7 +3,16 @@ import { expect } from 'chai';
 import { reducerContainers } from '../reducer';
 import * as actions from '../actions';
 
-const { addContainer, removeContainer, updateContainer } = actions;
+const {
+  addContainer,
+  removeContainer,
+  updateContainer,
+  addContainerChild,
+  removeContainerChild
+} = actions;
+
+const id = 1;
+const child = 2;
 
 describe('reducerContainers', () => {
   it('should add a new container', () => {
@@ -50,5 +59,40 @@ describe('reducerContainers', () => {
         tabs: true
       }
     });
+  });
+  it('should add a components into a container', () => {
+    const state = {
+      [id]: {
+        components: []
+      }
+    };
+    const nextState = {
+      [id]: {
+        components: [child]
+      }
+    };
+
+    expect(
+      reducerContainers(state, addContainerChild(id, child))
+    ).to.deep.equal(
+      nextState
+    );
+  });
+  it('should remove a components of the container', () => {
+    const state = {
+      [id]: {
+        components: [child]
+      }
+    };
+    const nextState = {
+      [id]: {
+        components: []
+      }
+    };
+    expect(
+      reducerContainers(state, removeContainerChild(id, child))
+    ).to.deep.equal(
+      nextState
+    );
   });
 });
