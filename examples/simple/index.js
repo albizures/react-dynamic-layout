d453f14b05c38db0e1f9fc5b5b95b2279e924326
Jose Albizures <jalbizures@xoom.com>
2017-01-26 10:50:58 -0600

    docs(examples): simple example added

---
+++ examples/simple/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { Layout, Register, Container, ROW, COLUMN } from '../../src';
+import Label from '../components/Label';
+
+export default () => <Layout name='Main' type={COLUMN}>
+  <Container size={50}>
+    <Register type={Label} props={{ text: 'Left' }}/>
+  </Container>
+  <Container size={50} tabs={false}>
+    <Layout type={ROW} name='Right'>
+      <Container size={50}>
+        <Register type={Label} props={{ text: 'Right top - tab 1' }}/>
+        <Register type={Label} props={{ text: 'Right top - tab 2' }}/>
+      </Container>
+      <Container size={50} >
+        <Register type={Label} props={{ text: 'Right bottom - tab 1' }}/>
+        <Register type={Label} props={{ text: 'Right bottom - tab 2' }}/>
+      </Container>
+    </Layout>
+  </Container>
+</Layout>;
