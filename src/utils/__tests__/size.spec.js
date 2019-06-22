7f7ce823f17424f0cbaeae8e2273771ed637b623
Jose Albizures <albizures3601@gmail.com>
2019-06-21 19:09:13 -0600

    fix module size tests

--- src/utils/__tests__/size.spec.js
+++ src/utils/__tests__/size.spec.js
@@ -1,36 +1,25 @@
-import { getDiff, getPercent } from '../size';
+// @ts-check
+import { dimensionsAreZero } from '../size';
 
 describe('size', () => {
-  it('should get the diff', () => {
-    const firstSize = {
-      width: 100,
-      height: 100,
-    };
-    const secondSize = {
-      width: 150,
-      height: 150,
-    };
-    const diff = {
-      width: 50,
-      height: 50,
-    };
-
-    expect(getDiff(firstSize, secondSize)).toEqual(diff);
-  });
-  it('should get the size in percentages', () => {
-    const totalSize = {
-      width: 200,
-      height: 200,
-    };
-    const partSize = {
-      width: 100,
-      height: 100,
-    };
-    const percent = {
-      width: 0.5,
-      height: 0.5,
-    };
-
-    expect(getPercent(partSize, totalSize)).toEqual(percent);
+  describe('#dimensionsAreZero', () => {
+    it('should check if all the dimensions are zero', () => {
+      expect(
+        dimensionsAreZero({
+          width: 0,
+          height: 0,
+          lastHeight: 40,
+          lastWidth: 40,
+        }),
+      ).toBe(true);
+      expect(
+        dimensionsAreZero({
+          width: 10,
+          height: 0,
+          lastHeight: 40,
+          lastWidth: 40,
+        }),
+      ).toBe(false);
+    });
   });
 });
